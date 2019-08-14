3a15a194eff994e5546f087b7a32c265d06de003
JohnnieWooker <lukish14@wp.pl>
2019-08-14 16:05:24 +0200

    Initial commit

---
+++ ControlPointsMerger/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ControlPointsMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 316);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Merge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 334);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Replace path";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "path";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 357);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 28);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 421);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Control Point Merger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ControlPointsMerger/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ControlPointsMerger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                foreach (string path in fileList)
+                { 
+                if (File.Exists(path))
+                {
+                    listBox1.Items.Add(path);
+                }
+                }
+            }
+            catch { }
+
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            listBox1.Enabled = false;
+            if (listBox1.Items.Count > 0)
+            {
+                int finalindex = 0;
+                string mergedlines = "";
+                string filesuffix = "final.csv";
+                List<string> filepaths = new List<string>();
+                List<int> pointindexes = new List<int>();
+                string dir = GetMasterDir();
+                foreach (string s in listBox1.Items) filepaths.Add(s);
+                foreach (string s in filepaths)
+                {
+                    if (File.Exists(s))
+                    {
+                        int localpointindex = 0;
+                        int newpointindex = 0;
+                        StreamReader file = new StreamReader(s);
+                        string line;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            string searchbounded = "";
+                            try
+                            {
+                                searchbounded = line.Substring(line.IndexOf(",") + 1);
+                                searchbounded = searchbounded.Substring(0, searchbounded.IndexOf(","));
+                                //MessageBox.Show(searchbounded);
+                                if (searchbounded.IndexOf("point ") >= 0)
+                                {
+                                    searchbounded = searchbounded.Substring(searchbounded.IndexOf("point ") + 6);
+                                    localpointindex = Convert.ToInt32(searchbounded);
+                                    if (newpointindex != localpointindex) finalindex++;
+                                    newpointindex = localpointindex;
+                                    if (checkBox1.Checked)
+                                    {
+                                        try
+                                        {
+                                            string toreplace = line.Substring(0, line.LastIndexOf("\\"));
+                                            string newreplace = textBox1.Text;
+                                            newreplace=newreplace.Replace("/", "\\");
+                                            //MessageBox.Show(newreplace[newreplace.Length - 1].ToString());
+                                            if (newreplace.LastIndexOf("\\")==newreplace.Length-1) newreplace = newreplace.Substring(0, newreplace.Length - 1);
+                                            line = line.Replace(toreplace, newreplace);
+                                        }
+                                        catch { }
+                                            }
+                                    mergedlines += line.Replace("point " + localpointindex.ToString(), "point " + finalindex) + "\r\n";
+                                   
+                                }
+
+                            }
+                            catch { }
+
+                        }
+                        finalindex++;
+                        // MessageBox.Show(newpointindex.ToString());
+
+                    }
+
+
+                }
+
+                string finalfile = dir + filesuffix;
+                if (File.Exists(finalfile)) File.Delete(finalfile);
+                File.WriteAllText(finalfile, mergedlines);
+            }
+            button1.Enabled = true;
+            listBox1.Enabled = true;
+        }
+
+        string GetMasterDir()
+        {
+            string dir = "";
+            if (listBox1.Items.Count>0)
+            {
+                dir = listBox1.Items[0].ToString();
+                dir=dir.Substring(0, dir.LastIndexOf("\\")+1);
+            }
+            return dir;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) { panel1.Visible = true; panel1.Size = new Size(237, 28);button1.Location = new Point(button1.Location.X, button1.Location.Y + 30); this.Size = new Size(this.Size.Width, this.Size.Height + 30); }
+            else { panel1.Visible = false; panel1.Size = new Size(237, 0); button1.Location = new Point(button1.Location.X, button1.Location.Y - 30); this.Size = new Size(this.Size.Width, this.Size.Height - 30); }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false; panel1.Size = new Size(237, 0); button1.Location = new Point(button1.Location.X, button1.Location.Y - 30); this.Size = new Size(this.Size.Width, this.Size.Height - 30);
+        }
+    }
+}
